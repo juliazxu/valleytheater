8204b9033c53b832215065fd06f93e32f15793cb
Julia Xu <zx278@nyu.edu>
2014-10-01 21:38:43 +0400

    first update yay

--- nyt.js
+++ nyt.js
@@ -1,52 +1,46 @@
 //define functions and variables at top
 var YTkey = 'AIzaSyDNjLfwF-2d4-XHUd4PsMB49cU4LLejOxY';
 
-function searchNYT(word){
-	console.log(word);
-
+//defining NYT Movie Reviews API function to search their database
+function searchNYT(word, callback){
 	var NYTURL = 'http://api.nytimes.com/svc/movies/v2/reviews/search.jsonp?query&api-key=42063812dc9ae09594a07d4c014974ad:10:69849853&query=' + word;
 
 	$.ajax({
 		url: NYTURL,
 		type: 'GET',
 		dataType: 'jsonp',
-		error: function(data){
-			console.log("Oh no! Didn't work...");
-		},
 		success: function(data){
-			console.log("Whoohoo!!");
-			console.log(data);
+			$('#loading').css("display", "none"); //displays loading gif when loading
+			$('#theResults').css("display", "block"); //loads results when loaded
+			if (data.num_results == '0'){
+				$('#theResults').slickAdd("<div><h4>" + "NO RESULTS. TRY AGAIN." + "</h4></div>");
 
-			console.log("The Title");
-			console.log(data.results[0].display_title);
-
-			console.log("The Headline");
-			console.log(data.results[0].headline);
-
-			console.log("The Release Date");
-			console.log(data.results[0].dvd_release_date);
-
-			console.log("The Trailer");
-			console.log(data.results[0].related_urls[4].suggested_link_text);
-
-			var theSearchedTerm = data.results[0].display_title;
-			var summary = data.results[0].summary_short;
-			var theSearchedResults = data.results[0].headline;
-			var DVDdate = data.results[0].dvd_release_date;
-			var TrailerURL = data.results[0].related_urls[4].url;
-
-			$('#theResults').html('<br>' + '<h4>' + theSearchedTerm + '</h4>' + '<h5>' + "SUMMARY: " + summary + '</h5><h3><i>' + "REVIEWER SENTIMENT: " + theSearchedResults + '</i></h3><h3>' + "RELEASED ON: " + DVDdate + '</h3>');
-			makeYoutubeRequest(theSearchedTerm);
+			} 
+			else {
+				console.log(data.results);
+				max = data.results.length > 5 ? 5 : data.results.length;
+				for (i=0; i<max; i++){
+					res = data.results[i];
+					var theSearchedTerm = res.display_title;
+					var summary = res.summary_short;
+					var theSearchedResults = res.headline;
+					if (summary != ""){
+					html = '<div>' + '<br>' + '<h4>' + theSearchedTerm + '</h4>' + '<h5>' + "SUMMARY: " + summary + '</h5><h3><i>' + "REVIEWER SENTIMENT: " + theSearchedResults + '</i></h3><h3><br>';
+					makeYoutubeRequest(theSearchedTerm, html); //runs second API function to query corresponding YouTube video
+					}
+				}	
+				
+			}	
 		}
 	});
 }
 
-function makeYoutubeRequest(word){
+function makeYoutubeRequest(word, html){
+
 	var url = 'https://www.googleapis.com/youtube/v3/search?';
-	var myParams = 'part=snippet&q=' + word + '+film' + '&type=video&order=viewCount&key=';
+	var myParams = 'part=snippet&q=' + word + '+film' + '&type=video&order=relevance&key=';
 	var myKey = YTkey;
 	var myURL = url + myParams + myKey;
-	console.log(word);
 
 	$.ajax({
 		url: myURL,
@@ -64,19 +58,24 @@ function makeYoutubeRequest(word){
 			var theVideoId = data.items[0].id.videoId;
 			console.log(theVideoId);
 			var embedString = '<iframe width="420" height="315" src="//www.youtube.com/embed/' + theVideoId + '" frameborder="0" allowfullscreen></iframe>';
-			$('#theResults').append(embedString + '<br><br><br><br>');
-
+			$('#theResults').slickAdd(html + embedString + '<br><br><br></div>');
 		}
 	});
 }
 
 $(document).ready(function(){
 	console.log("We are loaded!");
+	$('#theResults').slick();
 
 	//button listener
 	$('#theButton').click(function(){
 
-		console.log("Clicked the button!");
+		//displays loading gif instead of empty results box
+		$('#loading').css("display", "initial");
+		$('#theResults').css("display", "none");
+		for (i=0;i<4;i++){
+			$('#theResults').slickRemove(0);
+		}
 		var searchTerm = $('#theInput').val();
 		
 		//function to make NYT API request
@@ -84,7 +83,4 @@ $(document).ready(function(){
 
 		});
 
-
 });
-
-console.log("Not ready!");
